49e65ff9b4e5b4d12fedcf28b164be6b248ba343
sorena <mdn1376@gmail.com>
2024-08-13 12:01:48 +0330

    feat: made sender

---
+++ Tutorial_2/sender.py
@@ -0,0 +1,21 @@
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='localhost'))
+
+ch_1 = connection.channel()
+
+ch_1.queue_declare(queue='first', durable=True)
+
+message = input('enter message: ')
+
+ch_1.basic_publish(
+    exchange='',
+    routing_key='first',
+    body=message,
+    properties=pika.BasicProperties(
+        delivery_mode=2,  # 2 for "persistent", 1 for "transient".
+    )
+)
+print(f" [x] Sent message {message}")
+
+connection.close()
